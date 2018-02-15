8fca053f44faf82b70b52295227d29fb9d168ee3
Ravondo <Ravondo@users.noreply.github.com>
2018-02-15 15:07:12 -0400

    text.cpp

---
+++ text.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+#include <fstream>
+
+int main( )
+{
+   //create some data
+   short int a = -6730;
+   float b = 68.123; 
+   char c = 'J';
+   
+   ofstream myfile {"abc.txt"};
+   if (myfile)
+   {
+	   myfile << a << " " << b << " " << c;
+   } 
+   else
+   cout << "error opening abc.txt";
+   myfile.close();
+ 
+   cout<<a<<" "<<b<<" "<<c;//display data to console (i.e. as formatted chars)
+
+   cout<<endl<<endl;
+ 
+   return 0;
+}
